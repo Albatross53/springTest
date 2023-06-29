c26db66bc20ca7b5e838525f63da8a4863f5d059
전혜빈 <80448017+Albatross53@users.noreply.github.com>
2023-06-29 15:29:05 +0900

    Delete Testing directory

--- Testing/src/main/java/com/Jpa/Testing/MemberRepositoy.java
+++
@@ -1,6 +0,0 @@
-package com.Jpa.Testing;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepositoy extends JpaRepository<Member, Long> {
-}
--- Testing/src/main/java/com/Jpa/Testing/QuestionRepositoy.java
+++
@@ -1,6 +0,0 @@
-package com.Jpa.Testing;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepositoy extends JpaRepository<Question, Long> {
-}
